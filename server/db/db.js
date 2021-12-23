fe6eab6448560aaa3117245620b58d95f4e554af
hemhem <takutookamoto0427@gmail.com>
2021-12-23 11:42:49 -0500

    Connect with local PostgreSQL

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,22 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-  logging: false
-});
+const db = new Sequelize(
+  process.env.DATABASE_URL ||
+    `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@localhost:5432/messenger`,
+  {
+    logging: false,
+  }
+);
+
+// Database Connection Test
+async function connectionTest() {
+  try {
+    await db.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+}
+connectionTest();
 
 module.exports = db;
